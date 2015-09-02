8aad4ca87a5349e0b7c72bccfb7cafee51ae24e2
cw0 <element4emerica@gmail.com>
2015-09-02 15:17:40 -0400

    added variables for the list of files to install and current node version to use, added nvm installation. Needs testing

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@
 #Description: Initial server setup script since it is awful reinstalling linux
 #Assumptions: Ubuntu Server 14.04
 
+#init variables
+INSTALL_LIST = "git tmux vim"
+NODE_VERSION = 0.10.32
+
 #begin setup
 echo "Everything broken again? Don't worry I got you."
 
@@ -22,9 +26,25 @@ echo "Done."
 
 #install basic dev tools
 echo "Installing basic development tools..."
+sudo apt-get install $INSTALL_LIST
+echo "Done."
 
+#install nvm
+echo "Installing NVM..."
+git clone https://github.com/creationix/nvm.git ~/.nvm &&
+echo "source ~/.nvm/nvm.sh" >> ~/.bashrc
+echo "source ~/.nvm/nvm.sh" >> ~/.profile
+source ~/.nvm/nvm.sh
+nvm install $NODE_VERSION
+nvm alias default $NODE_VERSION
+echo "Done."
+
+echo "Updating NPM..."
+npm update -g --verbose
 echo "Done."
 
 #exit the script
-echo "All Done Here."
+echo "All Done Here. Press Any Key To Reboot..."
+read FINAL_INPUT
+sudo reboot
 exit
