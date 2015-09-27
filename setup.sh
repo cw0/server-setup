d22b77e51b4a1346472de9eb05f5688e4867743f
cw0 <element4emerica@gmail.com>
2015-09-27 14:33:59 -0400

    added in nvm commands

--- setup.sh
+++ setup.sh
@@ -34,6 +34,13 @@ echo "Installing NVM..."
 git clone https://github.com/creationix/nvm.git ~/.nvm &&
 echo "source ~/.nvm/nvm.sh" >> ~/.bashrc &&
 echo "source ~/.nvm/nvm.sh" >> ~/.profile &&
+. ~/.nvm/nvm.sh &&
+nvm install $NODE_VERSION &&
+nvm alias default $NODE_VERSION &&
+echo "Done." &&
+
+#update nvm
+npm update -g --verbose &&
 echo "Done."
 
 #exit the script
